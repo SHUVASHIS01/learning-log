7bfc610f02c804a351b5dfeb168d341c376f2c90
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-02 12:18:00 +0600

    Add initial variables and console log in type.js

---
+++ type.js
@@ -0,0 +1,6 @@
+var name = "Argho Basak";
+var age = 22;
+var isStudent = true;
+var height = 5.9;
+console.log(name, age, isStudent, height);
+console.log(typeof name, typeof age, typeof isStudent, typeof height);
