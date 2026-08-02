d0e39ec4dd0d4735ec4ac8cb3209771d17182997
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-02 11:59:51 +0600

    Initial commit

---
+++ variable.js
@@ -0,0 +1,9 @@
+/**
+ * 5 things to declare a variable in JavaScript
+ * 
+ */
+var weight = 70; // using var keyword
+var price = 100; // using var keyword
+var age;
+price = 200; // assigning value to price variable
+console.log(weight, price, age);
