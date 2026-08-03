64e90e7693645569341f6b6514fbb312c75d6500
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-03 13:09:39 +0600

    Add examples of accessing properties and values in objects; include school object with nested properties and events

--- Javascript/object.js
+++ Javascript/object.js
@@ -40,3 +40,21 @@ const computer = {
 const keys = Object.keys(computer); // Get an array of keys from the object
 console.log(keys); // Output: ['brand', 'model', 'price', 'processor', 'ram', 'storage']
 
+const values = Object.values(computer); // Get an array of values from the object
+console.log(values); // Output: ['Dell', 'XPS 13', 1200, 'Intel Core i7', '16GB', '512GB SSD']
+
+const school = {
+    name: 'Brac University',
+    classes: ['CSE', 'EEE', 'BBA', 'LAW'],
+    events: ['science fair', 'sports day', 'cultural fest'],
+    speciality: {
+        research: 'high quality research',
+        faculty: 'experienced faculty',
+        campus: 'modern campus'
+    }
+}
+console.log(school.name); // Accessing the name property
+console.log(school.speciality.research); // Accessing the research property inside the speciality object
+console.log(school.events[1]); // Accessing the second event in the events array
+
+
