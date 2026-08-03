07070ed44f1b4a6a7569932a2beeaf06f9a9542e
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-03 12:29:16 +0600

    Add string manipulation examples including slice, join, concat, and includes methods in string.js

--- Javascript/string.js
+++ Javascript/string.js
@@ -25,3 +25,46 @@ else {
     console.log('not same');
 }
 
+//Slice method
+const str = 'Hello, World!';
+const slicedStr = str.slice(7, 12);
+console.log(slicedStr); // Output: "World"
+
+const str1 = 'JavaScript is awesome!';
+console. log(str1.split(' ')); // Output: ["JavaScript", "is", "awesome!"]
+
+const str2 = 'i am a hard worker';
+console.log(str2.split('a')); // Output: ["i", "am", "", "hard w", "rker"]
+
+const array = ['Hello', 'World', 'JavaScript'];
+const joinedStr = array.join(' || ');
+console.log(joinedStr); // Output: "Hello, World, JavaScript"
+//concat method
+const first = 'Hello';
+const second = 'World';
+
+const fullname = first+ ' '+ second;
+// console.log(fullname); // Output: "Hello World"
+
+const fullname1 = first.concat(' ', second);
+console.log(fullname1); // Output: "Hello World"
+
+//includes method
+const str3 = 'JavaScript is awesome!';
+console.log(str3.includes('awesome'));
+
+//ultay dewa using loop
+let reverse = '';
+const str4 = 'JavaScript is awesome!';
+for (num of str4){
+    reverse = num + reverse;
+}
+console.log(reverse); // Output: "!emosewa si tpircSavaJ"
+
+//Another way to reverse a string using loop
+reverse = '';
+for (let i = str4.length - 1; i >= 0; i--) {
+    reverse += str4[i];
+}
+console.log(reverse); // Output: "!emosewa si tpircSavaJ"
+
