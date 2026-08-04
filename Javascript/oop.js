814c3ce9dba1543ad66f2654fabc5c2eb089e481
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-04 16:35:09 +0600

    Add encapsulation, private fields, getters/setters, and inheritance examples in JavaScript

--- Javascript/oop.js
+++ Javascript/oop.js
@@ -27,7 +27,7 @@ person1.greet();
 class Person2 {
 
     constructor(name, age) {
-        this.name = name;
+        this.name = name; //this means "the current object."
         this.age = age;
     }
 
@@ -38,4 +38,96 @@ class Person2 {
 }
 
 const p1 = new Person2("John", 20);
+console.log(p1)
 console.log(p1.name, p1.age)
+p1.greet();
+
+
+//Encapsulation : Bundling data and methods together.
+class BankAccount {
+
+    constructor(balance) {
+        this.balance = balance;
+    }
+
+    deposit(amount) {
+        return this.balance += amount;
+    }
+
+}
+const p2 = new BankAccount(300);
+console.log(p2)
+console.log(p2.deposit(89))
+console.log(p2.balance)
+
+//Private Fields
+// JavaScript supports true private variables using #.
+
+class BankAccount1 {
+
+    #balance = 0;
+    deposit(amount) {
+        this.#balance += amount;
+    }
+
+    show() {
+        console.log(this.#balance);
+    }
+
+}
+
+const account = new BankAccount1();
+
+account.deposit(500);
+
+account.show();
+//console.log(account.#balance); error dibe karo variable private: baire theke aaccess kora jabe na 
+
+// getter and setter 
+class Person3 {
+
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+}
+
+const p = new Person3("John");
+
+console.log(p.name);
+p.name = "Alice";
+
+console.log(p.name);
+
+
+//Inheritance : One class inherits another.
+class Animal {
+
+    eat() {
+        console.log("Eating");
+    }
+
+}
+
+class Dog extends Animal {
+
+    bark() {
+        console.log("Woof");
+    }
+
+}
+
+const dog = new Dog();
+
+dog.eat();
+
+dog.bark();
+
