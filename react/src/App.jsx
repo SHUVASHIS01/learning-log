7e341f3b7e71281ff6b2c0532a0649203dd33730
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-09 11:11:40 +0600

    feat: restructure components and add Profile and Gallery components with image handling

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,82 @@
-function Profile() {
-  return (
-    <img
-      src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+// function Profile() {
+//   return (
+//     <img
+//       src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   );
+// }
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+
+// import Profile from './Profile.jsx';
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// export default function TodoList() {
+//   return (
+//     <>
+//       <h1>Hedy Lamarr's Todos</h1>
+//       <img
+//         src="https://react.dev/images/docs/scientists/yXOvdOSs.jpg"
+//         alt="Hedy Lamarr"
+//         className="photo"
+//       />
+//       <ul>
+//         <li>Invent new traffic lights</li>
+//         <li>Rehearse a movie scene</li>
+//         <li>Improve spectrum technology</li>
+//       </ul>
+//     </>
+//   );
+// }
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src="https://react.dev/images/docs/scientists/7vQD0fPs.jpg"
+//         alt="Gregorio Y. Zara"
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+
+
+
