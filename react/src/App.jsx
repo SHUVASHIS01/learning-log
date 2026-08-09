9c856bbfec4ca424726923a0f230eab9c4473be1
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-09 13:52:40 +0600

    feat: add various Button component implementations and examples for user interaction

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -205,17 +205,129 @@ const person = {
 //   );
 // }
 
-export default function Button() {
-  function handleClick() {
-    alert('You clicked me!');
-  }
+// export default function Button() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
 
-  return (
-    <button onClick={handleClick}>
-      Click me
-    </button>
-  );
-}
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+//inline 
+
+// export default function Button() {
+//   return (
+//     <button
+//       onClick={function handleClick() {
+//         alert('You clicked me!');
+//       }}
+//     >
+//       Click me
+//     </button>
+//   );
+// }
+
+//another way 
+// export default function Button() {
+//   return (
+//     <button onClick={() => {
+//   alert('You clicked me!');
+// }}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+// function AlertButton({ message, children }) {
+//   return (
+//     <button onClick={() => alert(message)}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <AlertButton message="Playing!">
+//         Play Movie
+//       </AlertButton>
+//       <AlertButton message="Uploading!">
+//         Upload Image
+//       </AlertButton>
+//     </div>
+//   );
+// }
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// function PlayButton({ movieName }) {
+//   function handlePlayClick() {
+//     alert(`Playing ${movieName}!`);
+//   }
+
+//   return (
+//     <Button onClick={handlePlayClick}>
+//       Play "{movieName}"
+//     </Button>
+//   );
+// }
+
+// function UploadButton() {
+//   return (
+//     <Button onClick={() => alert('Uploading!')}>
+//       Upload Image
+//     </Button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <PlayButton movieName="Kiki's Delivery Service" />
+//       <UploadButton />
+//     </div>
+//   );
+// }
+
+// function Button({ onSmash, children }) {
+//   return (
+//     <button onClick={onSmash}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Button onSmash={() => alert('Playing!')}>
+//         Play Movie
+//       </Button>
+//       <Button onSmash={() => alert('Uploading!')}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+//Event propagation 
+
+
+
+
 
 
 
