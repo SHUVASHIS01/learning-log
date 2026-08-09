c3c7d87d3e9cccf1a0c173e34641e922d2d80a3f
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-09 14:21:39 +0600

    fix: enable LightSwitch component to toggle background color

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -379,22 +379,26 @@ const person = {
 
 //Challenge 1 of 2: Fix an event handler 
 // Clicking this button is supposed to switch the page background between white and black. However, nothing happens when you click it. Fix the problem. (Don’t worry about the logic inside handleClick—that part is fine.)
-export default function LightSwitch() {
-  function handleClick() {
-    let bodyStyle = document.body.style;
-    if (bodyStyle.backgroundColor === 'black') {
-      bodyStyle.backgroundColor = 'white';
-    } else {
-      bodyStyle.backgroundColor = 'black';
-    }
-  }
+// export default function LightSwitch() {
+//   function handleClick() {
+//     let bodyStyle = document.body.style;
+//     if (bodyStyle.backgroundColor === 'black') {
+//       bodyStyle.backgroundColor = 'white';
+//     } else {
+//       bodyStyle.backgroundColor = 'black';
+//     }
+//   }
 
-  return (
-    <button onClick={()=> handleClick()}>
-      Toggle the lights
-    </button>
-  );
-}
+//   return (
+//     <button onClick={()=> handleClick()}>
+//       Toggle the lights
+//     </button>
+//   );
+// }
+
+
+//State: A Component's Memory
+
 
 
 
