476f402f8c8b6062bb89f2cd39fb58d042cb2501
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-11 16:23:37 +0600

    refactor: reorganize RequestTracker and add List component for managing artists

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -658,41 +658,294 @@
 // }
 
 //Challenge 1 of 2: Fix a request counter
+// import { useState } from 'react';
+
+// export default function RequestTracker() {
+//   const [pending, setPending] = useState(0);
+//   const [completed, setCompleted] = useState(0);
+
+//   async function handleClick() {
+//     setPending(p => p + 1);
+//     await delay(3000);
+//     setPending(p => p - 1);
+//     setCompleted(c => c + 1);
+//   }
+
+//   return (
+//     <>
+//       <h3>
+//         Pending: {pending}
+//       </h3>
+//       <h3>
+//         Completed: {completed}
+//       </h3>
+//       <button onClick={handleClick}>
+//         Buy
+//       </button>
+//     </>
+//   );
+// }
+
+// function delay(ms) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, ms);
+//   });
+// }
+
+
+// import { useState } from 'react';
+
+// export default function MovingDot() {
+//   const [position, setPosition] = useState({
+//     x: 0,
+//     y: 0
+//   });
+//   return (
+//     <div
+//       onPointerMove={e => {
+//         setPosition({
+//           x: e.clientX,
+//           y: e.clientY
+//         });
+//       }}
+//       style={{
+//         position: 'relative',
+//         width: '100vw',
+//         height: '100vh',
+//       }}>
+//       <div style={{
+//         position: 'absolute',
+//         backgroundColor: 'red',
+//         borderRadius: '50%',
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         left: -10,
+//         top: -10,
+//         width: 20,
+//         height: 20,
+//       }} />
+//     </div>
+//   );
+// }
+
+// import { useState } from 'react';
+
+// export default function Form() {
+//   const [person, setPerson] = useState({
+//     firstName: 'Barbara',
+//     lastName: 'Hepworth',
+//     email: 'bhepworth@sculpture.com'
+//   });
+
+//   function handleFirstNameChange(e) {
+//     setPerson({
+//       ...person,
+//       firstName: e.target.value
+//     });
+//   }
+
+//   function handleLastNameChange(e) {
+//     setPerson({
+//       ...person,
+//       lastName: e.target.value
+//     });
+//   }
+
+//   function handleEmailChange(e) {
+//     setPerson({
+//       ...person,
+//       email: e.target.value
+//     });
+//   }
+
+//   return (
+//     <>
+//       <label>
+//         First name:
+//         <input
+//           value={person.firstName}
+//           onChange={handleFirstNameChange}
+//         />
+//       </label>
+//       <label>
+//         Last name:
+//         <input
+//           value={person.lastName}
+//           onChange={handleLastNameChange}
+//         />
+//       </label>
+//       <label>
+//         Email:
+//         <input
+//           value={person.email}
+//           onChange={handleEmailChange}
+//         />
+//       </label>
+//       <p>
+//         {person.firstName}{' '}
+//         {person.lastName}{' '}
+//         ({person.email})
+//       </p>
+//     </>
+//   );
+// }
+
+// import { useState } from 'react';
+
+// export default function Form() {
+//   const [person, setPerson] = useState({
+//     name: 'Niki de Saint Phalle',
+//     artwork: {
+//       title: 'Blue Nana',
+//       city: 'Hamburg',
+//       image: 'https://react.dev/images/docs/scientists/Sd1AgUOm.jpg',
+//     }
+//   });
+
+//   function handleNameChange(e) {
+//     setPerson({
+//       ...person,
+//       name: e.target.value
+//     });
+//   }
+
+//   function handleTitleChange(e) {
+//     setPerson({
+//       ...person,
+//       artwork: {
+//         ...person.artwork,
+//         title: e.target.value
+//       }
+//     });
+//   }
+
+//   function handleCityChange(e) {
+//     setPerson({
+//       ...person,
+//       artwork: {
+//         ...person.artwork,
+//         city: e.target.value
+//       }
+//     });
+//   }
+
+//   function handleImageChange(e) {
+//     setPerson({
+//       ...person,
+//       artwork: {
+//         ...person.artwork,
+//         image: e.target.value
+//       }
+//     });
+//   }
+
+//   return (
+//     <>
+//       <label>
+//         Name:
+//         <input
+//           value={person.name}
+//           onChange={handleNameChange}
+//         />
+//       </label>
+//       <label>
+//         Title:
+//         <input
+//           value={person.artwork.title}
+//           onChange={handleTitleChange}
+//         />
+//       </label>
+//       <label>
+//         City:
+//         <input
+//           value={person.artwork.city}
+//           onChange={handleCityChange}
+//         />
+//       </label>
+//       <label>
+//         Image:
+//         <input
+//           value={person.artwork.image}
+//           onChange={handleImageChange}
+//         />
+//       </label>
+//       <p>
+//         <i>{person.artwork.title}</i>
+//         {' by '}
+//         {person.name}
+//         <br />
+//         (located in {person.artwork.city})
+//       </p>
+//       <img
+//         src={person.artwork.image}
+//         alt={person.artwork.title}
+//       />
+//     </>
+//   );
+// }
+
+
+
+// import { useState } from 'react';
+
+// let nextId = 0;
+
+// export default function List() {
+//   const [name, setName] = useState('');
+//   const [artists, setArtists] = useState([]);
+
+//   return (
+//     <>
+//       <h1>Inspiring sculptors:</h1>
+//       <input
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//       <button onClick={() => {
+//         setArtists([
+//           ...artists,
+//           { id: nextId++, name: name }
+//         ]);
+//       }}>Add</button>
+//       <ul>
+//         {artists.map(artist => (
+//           <li key={artist.id}>{artist.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// push er bodole amra jevabe state er khetre kono elem array te add kori
+
 import { useState } from 'react';
 
-export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+let nextId = 0;
 
-  async function handleClick() {
-    setPending(p => p + 1);
-    await delay(3000);
-    setPending(p => p - 1);
-    setCompleted(c => c + 1);
-  }
+// export default function List() {
+//   const [name, setName] = useState('');
+//   const [artists, setArtists] = useState([]);
 
-  return (
-    <>
-      <h3>
-        Pending: {pending}
-      </h3>
-      <h3>
-        Completed: {completed}
-      </h3>
-      <button onClick={handleClick}>
-        Buy
-      </button>
-    </>
-  );
-}
-
-function delay(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
-
-
+//   return (
+//     <>
+//       <h1>Inspiring sculptors:</h1>
+//       <input
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//       <button onClick={() => {
+//         setArtists([
+//           ...artists,
+//           { id: nextId++, name: name }
+//         ]);
+//       }}>Add</button>
+//       <ul>
+//         {artists.map(artist => (
+//           <li key={artist.id}>{artist.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
 
 
 
