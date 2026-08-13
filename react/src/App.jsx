e2dc6a1766694154d0bdde2bc713e20cbc8c420d
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-13 12:50:35 +0600

    feat: add toggle functionality to render a second Counter component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1558,14 +1558,59 @@
 // }
 
 // state ekta component er moddhe tied thake na, rather react tree te thake
+// import { useState } from 'react';
+
+// export default function App() {
+//   const counter = <Counter />;
+//   return (
+//     <div>
+//       {counter}
+//       {counter}
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [score, setScore] = useState(0);
+//   const [hover, setHover] = useState(false);
+
+//   let className = 'counter';
+//   if (hover) {
+//     className += ' hover';
+//   }
+
+//   return (
+//     <div
+//       className={className}
+//       onPointerEnter={() => setHover(true)}
+//       onPointerLeave={() => setHover(false)}
+//     >
+//       <h1>{score}</h1>
+//       <button onClick={() => setScore(score + 1)}>
+//         Add one
+//       </button>
+//     </div>
+//   );
+// }
+
 import { useState } from 'react';
 
 export default function App() {
-  const counter = <Counter />;
+  const [showB, setShowB] = useState(true);
   return (
     <div>
-      {counter}
-      {counter}
+      <Counter />
+      {showB && <Counter />}
+      <label>
+        <input
+          type="checkbox"
+          checked={showB}
+          onChange={e => {
+            setShowB(e.target.checked)
+          }}
+        />
+        Render the second counter
+      </label>
     </div>
   );
 }
@@ -1592,7 +1637,6 @@ function Counter() {
     </div>
   );
 }
-
 
 
 
