dffa25f9a0cbf5dc0391f4395c119de374d9b2ba
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-16 14:17:14 +0600

    feat: add Form component with input focus functionality

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2259,3 +2259,24 @@
 // }
 
 // export default Stopwatch;
+
+
+import { useRef } from "react";
+
+export default function Form() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+
+      <button onClick={handleClick}>
+        Focus the input
+      </button>
+    </>
+  );
+}
