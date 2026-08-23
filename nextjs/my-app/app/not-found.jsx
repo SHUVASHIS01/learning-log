aec43a108a2f7e2290d59c5b06ca82e4c2a51940
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-23 17:04:43 +0600

    feat: implement Navbar component with dynamic link highlighting and add NotFound page

---
+++ nextjs/my-app/app/not-found.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+        <h2>page ta nai re..</h2>
+    </div>
+  );
+}
--- nextjs/my-app/components/navbar.jsx
+++ nextjs/my-app/components/navbar.jsx
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const navbar = () => {
+const Navbar = () => {
+  const pathName = usePathname();
     const links = <>
-    <li><Link href="/about">About</Link></li>
-    <li><Link href="/contact">Contact</Link></li>
-    <li><Link href="/blogs">Blogs</Link></li>
-    <li><Link href="/dashboard">Dashboard</Link></li>
-    <li><Link href="/users">Users</Link></li>
+    <li><Link className={pathName === '/' ? 'text-blue-700': ''} href="/">Home</Link></li>
+    <li><Link className={pathName === '/about' ? 'text-blue-700': ''} href="/about">About</Link></li>
+    <li><Link className={pathName === '/contact' ? 'text-blue-700': ''} href="/contact">Contact</Link></li>
+    <li><Link className={pathName === '/blogs' ? 'text-blue-700': ''} href="/blogs">Blogs</Link></li>
+    <li><Link className={pathName === '/dashboard' ? 'text-blue-700': ''} href="/dashboard">Dashboard</Link></li>
+    <li><Link className={pathName === '/users' ? 'text-blue-700': ''} href="/users">Users</Link></li>
     </>
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -34,4 +38,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
