6e69272590861689a816f6f2e247c1e8c99e1b08
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-23 17:24:58 +0600

    feat: add Roboto font styling to blogs page for improved typography

--- nextjs/my-app/app/blogs/page.jsx
+++ nextjs/my-app/app/blogs/page.jsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import { blogs } from "./blogs";
+import { Roboto } from "next/font/google";
 export const metadata = {
     title: "Blogs",
     description: "...",
 };
 
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function blogsPage() {
   return (
-    <div>
+    <div className={roboto.className}>
         <h2 className="text-3xl font-bold mb-4">Blogs</h2>
           {blogs.map(blog => <div key = {blog.id}>
            <h3>{blog.title}</h3>
