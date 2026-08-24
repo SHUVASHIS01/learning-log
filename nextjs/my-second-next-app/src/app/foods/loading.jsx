10fdec13682131c5f42d802269219490b29ac130
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-24 15:26:51 +0600

    feat: add error and loading components for food pages

---
+++ nextjs/my-second-next-app/src/app/foods/loading.jsx
@@ -0,0 +1,9 @@
+const foodLoading = () => {
+  return (
+    <div className= "flex items-center justify-center h-center">
+        <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+
+export default foodLoading;
