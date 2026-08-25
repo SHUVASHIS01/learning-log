91736ec3291a8246170dd198a1960fb91e62a26d
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-25 11:32:06 +0600

    feat: add global styles, layout, and header component for Next.js app
    - Created globals.css for styling with Tailwind CSS and DaisyUI
    - Implemented RootLayout in layout.js to include global styles and header
    - Developed Home page in page.js with responsive design and links
    - Added PostPage in posts/page.jsx to fetch and display posts from API
    - Introduced Header component for navigation with links to Home and Posts

---
+++ nextjs/next.js-data-fetch/src/app/posts/page.jsx
@@ -0,0 +1,21 @@
+
+const getPost = async () =>{
+    const res = await fetch ("https://jsonplaceholder.typicode.com/posts");
+    if (!res.ok){
+        throw new Error('Failed to fetch posts');
+    }
+    return res.json();
+}
+
+const PostPage =  async () => {
+//     const res = await fetch ("https://jsonplaceholder.typicode.com/posts");
+//   const posts = await res.json();
+        const posts = await getPost();
+  return (
+    <div>
+        <h2>post ashtese..{posts.length} gula</h2>
+    </div>
+  );
+};
+
+export default PostPage;
