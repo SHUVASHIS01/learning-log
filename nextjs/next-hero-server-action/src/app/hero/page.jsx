177a297cd8bd4cdd0b27147c418950218f79b65e
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-27 12:32:28 +0600

    Add global styles, layout, and hero components for Next.js app
    - Created globals.css for global styles including dark mode support.
    - Implemented layout.jsx to define the root layout with font imports.
    - Added hero/page.jsx with buttons from @heroui/react.
    - Developed home/page.jsx as the main landing page with Next.js logo and links.

---
+++ nextjs/next-hero-server-action/src/app/hero/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+export default function HeroPage() {
+  return (
+    <div>
+      <h2>Hero page</h2>
+      <Button variant="secondary">Hero Button</Button>
+      <Button variant="outline">Hero Button</Button>
+      <Button variant="ghost">Hero Button</Button>
+    </div>
+  );
+}
