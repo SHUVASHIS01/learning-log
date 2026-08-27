e41c2e3b80d6785566c4412f8d101e7ed5ce223d
SHUVASHIS01 <shuvashis.basak@g.bracu.ac.bd>
2026-08-27 14:01:43 +0600

    feat: add AddTask component and integrate it into TaskPage; update ThemeToggle with Switch component and icons

--- nextjs/next-hero-server-action/src/app/tasks/page.jsx
+++ nextjs/next-hero-server-action/src/app/tasks/page.jsx
@@ -1,13 +1,14 @@
 import TasksCard from "../../components/TasksCard";
 import { getTasks } from "../../lib/tasks";
-
+import AddTask from "../../components/AddTask";
 export default async function TaskPage() {
   const tasks = await getTasks();
 
   return (
     <div>
       <h2>Tasks: {tasks.length}</h2>
-      <div>
+      <AddTask></AddTask>
+      <div className = 'grid grid-cols-3 gap-4 pt-8'>
         {tasks.map((task) => (
           <TasksCard key={task.id} task={task}></TasksCard>
         ))}
--- nextjs/next-hero-server-action/src/components/ThemeTogle.jsx
+++ nextjs/next-hero-server-action/src/components/ThemeTogle.jsx
@@ -1,13 +1,27 @@
 "use client";
+import { Switch } from "@heroui/react";
+import Moon from "@gravity-ui/icons/Moon";
+import Sun from "@gravity-ui/icons/Sun";
 import { useTheme } from "../providers/NextThemeProvider";
+
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
     <div>
-      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        Toggle {theme === "dark" ? "Light" : "Dark"} Mode
-      </button>
+      <Switch
+        aria-label="Toggle dark mode"
+        isSelected={theme === "dark"}
+        onChange={(isSelected) => setTheme(isSelected ? "dark" : "light")}
+      >
+        <Switch.Content>
+          <Switch.Control>
+            <Switch.Thumb>
+              {theme === "dark" ? <Moon aria-hidden="true" /> : <Sun aria-hidden="true" />}
+            </Switch.Thumb>
+          </Switch.Control>
+        </Switch.Content>
+      </Switch>
     </div>
   );
 }
